cf6cd67aaf1945f8fadfecd25f1432303726e134
PascalPozzera <79970848+PascalPozzera@users.noreply.github.com>
2025-04-13 08:52:00 +0200

    init query-side, added docker-compose file for postgresql db

---
+++ hotel-query-side/src/main/java/fhv/query/controller/customer/CustomerQueryController.java
@@ -0,0 +1,29 @@
+package fhv.query.controller.customer;
+
+import at.fhv.sys.hotel.commands.shared.events.CustomerCreated;
+import fhv.projection.customer.CustomerProjection;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.jboss.logmanager.Logger;
+
+@Path("/api")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class CustomerQueryController {
+
+    @Inject
+    CustomerProjection customerProjection;
+
+    public CustomerQueryController() {
+    }
+
+    @POST
+    @Path("/customerCreated")
+    public Response customerCreated(CustomerCreated event) {
+        Logger.getAnonymousLogger().info("Received event: " + event);
+        customerProjection.processCustomerCreatedEvent(event);
+        return Response.ok(event).build();
+    }
+}
